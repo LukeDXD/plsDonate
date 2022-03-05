7b993f59d40acabbd2f3ea08e09b7a7043def808
LukeDXD <101000214+LukeDXD@users.noreply.github.com>
2022-03-05 11:48:49 +0000

    Add files via upload

---
+++ Robloxdonatepls.lua
@@ -0,0 +1,17 @@
+local textTable = {"👪______", "👪_____🚅", "👪____🚅_", "👪___🚅__", "👪__🚅___", "👪_🚅____", "👪🚅_____", "💥______", "You didn't save the family"}
+
+while wait(1) do
+
+for i, v in pairs(textTable) do
+
+wait(1.0)
+
+local args = {
+    [1] = v,
+    [2] = "booth"
+}
+
+game:GetService("ReplicatedStorage").Events.EditBooth:FireServer(unpack(args))
+
+end
+    end
